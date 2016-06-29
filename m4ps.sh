cd6069538e90860f20f56cbb6c90ef149c0deb3c
Joseph Harriott <trohib@gmail.com>
2016-06-29 08:06:40 +0200

    "non-zero first argument"

--- m4ps.sh
+++ m4ps.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # vim: set tw=0
 
-# Joseph Harriott http://momentary.eu/ Fri 12 Jun 2015
+# Joseph Harriott http://momentary.eu/ Wed 29 Jun 2016
 # Recursively find all *.md files in the current directory, convert those that haven't been done yet or have changed since last converted to pdf. Use LaTeX Chapter as the first level heading, and Subsubsection as the 4th (and preferably last) level heading. Apply some neater formatting.
 # ----------------------------------------------------------------------------------------------
-# If an argument is given, all of the pdf's are re-done.
+# If a non-zero first argument is given, all of the pdf's are re-done.
 
 # set the (Pandoc) ToC switches:
 if [ ! $2 ]; then tc="--toc --toc-depth=4"; fi
