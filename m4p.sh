37ad7455e929b579dec7bf0a80e46def3cb9031b
harriott <trohib@gmail.com>
2016-09-24 01:36:39 +0200

    Goldenrod

--- m4p.sh
+++ m4p.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # vim: set et tw=0:
 
-# Joseph Harriott http://momentary.eu/  Fri 23 Sep 2016
+# Joseph Harriott http://momentary.eu/  Thu 22 Sep 2016
 # Convert a single markdown file to pdf nicely.
 # ----------------------------------------------------
 
@@ -10,7 +10,7 @@ if [ $1 ]; then
     if [ -s $mdf.md ]; then
 
         # set the (Pandoc) ToC switches (the default case) if no 2nd argument was given:
-        if [ ! $2 ]; then tc="--toc --toc-depth=4"; fi
+        if [ ! $2 ]; then tc="--toc --toc-depth=5"; fi
 
         bash "$( dirname "${BASH_SOURCE[0]}" )/md4pdf.sh" $mdf "$tc"
 
