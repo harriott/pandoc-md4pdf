07c76c65706ba4d2355596e0b22b258ffa38495e
Joseph Harriott <trohib@gmail.com>
2016-09-29 15:05:59 +0200

    Write-Host

--- m4ps.ps1
+++ m4ps.ps1
@@ -6,32 +6,32 @@
 # --------------------------------------------------------------------------------------------------
 # If a non-zero first argument is given, all of the pdf's are re-done.
 # If a second argument is given, ToC is switched off.
+param( [string]$redo, [string]$ToCoff )
+if (!$ToCoff) {$ToC="--toc --toc-depth=5"}
 
 gci -r -i *.md| # get all the markdown files recursively
 foreach{
-	$fn=$_.basename
-	$md=$_.fullname
-	$mdt=$_.LastWriteTime
-	"$md -> $mdt" # print the markdown file's fullname with LastWriteTime
-	$gp=$false # set a "go pdf" boolean
-	$pdf=$_.directoryname+"\"+$fn+".pdf"
-	if (test-path "$pdf"){
-		gi "$pdf"|
-		foreach{
-			$pdft=$_.LastWriteTime;
-			if($pdft -gt $mdt){"$pdf > $pdft"} # and print the pdf's name & time
-			else{$gp=$true;"$pdf > $pdft - to redo"} # or signal it's to be redone
-			}
-		}
-	else{$gp=$true;"$pdf -- not yet made"} # no pdf, so signal it's to be done
-	if($gp){
-		# Prepare the Titling:
-		$Ln=$fn.replace("_","\_") # escape underscores for LaTeX
-		echo "\renewcommand\contentsname{$Ln} \renewcommand{\thechapter}{} \usepackage{titlesec} \titleformat{\chapter}{}{}{0em}{\bfseries\LARGE} \titlespacing{\chapter}{0pt}{30pt}{*2} \usepackage{xcolor} \makeevenfoot{plain}{}{\textcolor{lightgray}{$Ln} \quad p.\thepage\ }{} \makeoddfoot{plain}{}{\textcolor{lightgray}{$Ln} \quad p.\thepage\ }{}" |
-		out-file md4pdf.tex -encoding UTF8
-		"- running pandoc"
-		&pandoc -Vdocumentclass:memoir -Vclassoption:article -H md4pdf.tex -V mainfont:Arial --toc --toc-depth=4 -f markdown_strict $md -o $pdf --latex-engine=xelatex
-		rm md4pdf.tex # tidy up
-		}
-	}
+    $fn=$_.basename
+    $md=$_.fullname
+    $mdt=$_.LastWriteTime
+    "$md -> $mdt" # print the markdown file's fullname with LastWriteTime
+    $gp=$false # set a "go pdf" boolean
+    $pdf=$_.directoryname+"\"+$fn+".pdf"
+    if (test-path "$pdf"){
+        gi "$pdf"|
+        foreach{
+            $pdft=$_.LastWriteTime;
+            # print a newer pdf's name & time:
+            if($pdft -gt $mdt){Write-Host "$pdf > $pdft" -foreground Gray}
+            # or signal an old pdf is to be redone:
+            else{$gp=$true;Write-Host "$pdf > $pdft - to redo" -foreground Gray}
+            }
+        }
+    # no pdf, so signal it's to be done:
+    else{$gp=$true;Write-Host "$pdf -- not yet made" -foreground Gray}
+    if($gp){
+        Write-Host "- running pandoc" -foreground Gray
+        # PowerShell -NoProfile "$PSScriptRoot\md4pdf.ps1 $fn $toc"
+        }
+    }
 
