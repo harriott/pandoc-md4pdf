22fe4ce072306eda97d65113243aed224f072f42
Joseph Harriott <trohib@gmail.com>
2016-10-04 00:06:22 +0200

    replace('#','\#')

--- md4pdf.ps1
+++ md4pdf.ps1
@@ -1,5 +1,5 @@
 # vim: set et tw=0:
-# Joseph Harriott  http://momentary.eu/  Thu 29 Sep 2016
+# Joseph Harriott  http://momentary.eu/  Tue 04 Oct 2016
 # Engine to convert markdown file to pdf nicely.
 # ------------------------------------------------------
 # Call this from a wrapper: md4pdf.ps1 md-file-basename [pandoc-toc-settings]
@@ -11,13 +11,17 @@ $mdf="$mdbn.md"
 if (test-path "$mdf") {
     $giih="$PSScriptRoot\md4pdf-iih.tex" # the generic include-in-header file
 
-    # Generate the specific include-in-header file:
-    $texbn=$mdbn.replace('\','/').replace('_','\_') # prepares the filename for feeding to TeX
+    # Generate the specific include-in-header file
+    # --------------------------------------------
+    # prepare the filename for feeding to TeX
+    $texbn=$mdbn.replace('\','/').replace('_','\_').replace('#','\#')
+
     $iihLines="\renewcommand\contentsname{\normalsize $texbn}`r`n\cfoot{ {\textcolor{lightgray}{$texbn}} \quad p.\thepage\ of \pageref{LastPage}}"
     $iihf=$(gci "$mdf").fullname -replace '\.md$','-md4pdf-iih.tex'
     [IO.File]::WriteAllLines($iihf, $iihLines)
 
     # (try to) Pandoc
+    # ---------------
     pandoc -V subparagraph=yes -H $giih -H $iihf -V mainfont=Arial $toc $tocd -f markdown_strict "$mdf" -o "$mdbn.pdf" --latex-engine=xelatex
     ri $iihf # tidy up, anyway
 }else{
