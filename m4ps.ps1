2cefafc08af50dad8959ad47dfa686d213abdf83
Joseph Harriott <trohib@gmail.com>
2018-03-27 18:02:39 +0200

    changed to switch parameters

--- m4ps.ps1
+++ m4ps.ps1
@@ -1,13 +1,12 @@
 #!/bin/bash
 # vim: set et tw=0:
 
-# Joseph Harriott http://momentary.eu/  Wed 28 Sep 2016
+# Joseph Harriott http://momentary.eu/  Tue 27 Mar 2018
 # Recursively find all *.md files in the current directory, convert those that haven't been done yet or have changed since last converted to pdf. Use LaTeX Chapter as the first level heading, and Subsubsection as the 4th (and preferably last) level heading. Apply some neater formatting.
 # --------------------------------------------------------------------------------------------------
-# If a non-zero first argument is given, all of the pdf's are re-done.
-# If a second argument is given, ToC is switched off.
-param( [string]$redo, [string]$ToCoff )
-if (!$ToCoff) {$ToC="--toc --toc-depth=5"}
+# eg  m4ps -r -n  would redo all pdf's, and switch off ToC
+param( [switch]$redo, [switch]$noToC )
+if (!$noToC) {$ToC="--toc --toc-depth=5"}
 
 gci -r -i *.md| # get all the markdown files recursively
 foreach{
@@ -35,7 +34,7 @@ foreach{
     if($gp){
         Write-Host "- running pandoc" -foreground Gray
         $mdp = $md -replace '\.md$','' # md file's basepathname
-        PowerShell -NoProfile "$PSScriptRoot\md4pdf.ps1 $mdp $toc"
+        PowerShell -NoProfile "$PSScriptRoot\md4pdf.ps1 $mdp $toc" # finally does the work
         }
     }
 
